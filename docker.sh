6d8faf2d7d397613d294adab3a6142e098666f45
Rakesh K H <Rakeshkh@outlook.in>
2025-06-19 22:02:53 +0530

    expense app

--- docker.sh
+++ docker.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+growpart /dev/nvme0n1 4
+lvextend -l +50%FREE /dev/RootVG/rootVol
+lvextend -l +50%FREE /dev/RootVG/varVol
+xfs_growfs /
+xfs_growfs /var
 
 #docker 
 chmod +x docker.sh
